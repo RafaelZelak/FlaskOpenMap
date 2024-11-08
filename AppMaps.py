4abac4667a4e0d1746b92ff7d0421a74774fa847
RafaelZelak <rzf1503@gmail.com>
2024-11-08 16:11:23 -0300

    radio

--- AppMaps.py
+++ AppMaps.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 from geopy.geocoders import Nominatim
 from geopy.exc import GeocoderTimedOut
 import json
 import re
 
 app = Flask(__name__)
-
-# Inicializa o geolocalizador
 geolocator = Nominatim(user_agent="geoapi_exercises", timeout=10)
 
 # Função para formatar o endereço
@@ -30,33 +28,38 @@ def obter_coordenadas(endereco):
     return None, None
 
 # Processa os dados do arquivo JSON
-def processar_empresas():
+def carregar_empresas():
     with open('empresas.json', 'r', encoding='utf-8') as file:
-        empresas = json.load(file)
+        return json.load(file)
 
-    resultados = []
-    for empresa in empresas:
+empresas = carregar_empresas()
+
+@app.route('/')
+def index():
+    return render_template('indexMaps.html')
+
+@app.route('/api/empresas/total')
+def total_empresas():
+    return jsonify(total=len(empresas))
+
+@app.route('/api/empresa')
+def empresa():
+    index = int(request.args.get('id', 0))
+    if index < len(empresas):
+        empresa = empresas[index]
         logradouro = empresa.get("logradouro")
         if logradouro:
             endereco_formatado = formatar_endereco(logradouro)
             latitude, longitude = obter_coordenadas(endereco_formatado)
             if latitude and longitude:
-                resultados.append({
+                return jsonify({
                     "nome_fantasia": empresa.get("nome_fantasia", "Nome não disponível"),
                     "endereco": logradouro,
                     "telefone_1": empresa.get("telefone_1", "Telefone não disponível"),
                     "latitude": latitude,
                     "longitude": longitude
                 })
-    return resultados
-
-@app.route('/')
-def index():
-    return render_template('indexGps.html')
-
-@app.route('/api/empresas')
-def empresas():
-    return jsonify(processar_empresas())
+    return jsonify({}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5001)
+    app.run(debug=True)
